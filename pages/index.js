7f1c1770eb3c47f7195a5d8edc4ee250310763c6
Kevin Boyle <papershack@gmail.com>
2019-02-22 16:33:58 -0800

    set up index.js to fetch json data from hackernews api, render via .map

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,37 @@
+import fetch from 'isomorphic-fetch';
+import Error from 'next/error';
+
 class Index extends React.Component {
+    static async getInitialProps(){
+        let stories;
+        try {
+            const response = await fetch('https://node-hnapi.herokuapp.com/news?page=1');      
+            stories = await response.json();
+        } catch (err){
+            console.log(err);
+            stories = [];
+        }
+
+        return { stories };
+    }
+
     render(){
-        return(
-            <div>hacker next</div>
+        const { stories } = this.props;
+
+        if (stories.length === 0){
+            return <Error statusCode={503}/>
+        } 
+            return(
+            <div>
+                <div>
+                    <h1>HackerNext</h1>
+                </div>
+                <div>
+                    {stories.map(story => (
+                        <h2 key={story.id}>{story.title}</h2>
+                    ))};
+                </div>
+            </div>
         )
     }
 }
